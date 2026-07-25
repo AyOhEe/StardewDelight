154effd4824e4f00c79b6245f4ff9bed568ec1c6
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-07-25 18:55:10 +0100

    Crop properties

--- src/main/java/io/github/ayohee/stardewdelight/register/SDBlocks.java
+++ src/main/java/io/github/ayohee/stardewdelight/register/SDBlocks.java
@@ -24,55 +24,100 @@ public class SDBlocks {
     public static final DeferredBlock<Block> BLUE_JAZZ_CROP = block(
             "blue_jazz_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> CAULIFLOWER_CROP = block(
             "cauliflower_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> COFFEE_BUSH = block(
             "coffee_bush",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> GARLIC_CROP = block(
             "garlic_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> GREEN_BEANS_CROP = block(
             "green_beans_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> KALE_CROP = block(
             "kale_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> PARSNIP_CROP = block(
             "parsnip_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> RHUBARB_CROP = block(
             "rhubarb_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> STRAWBERRY_BUSH = block(
             "strawberry_bush",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
 
@@ -80,61 +125,111 @@ public class SDBlocks {
     public static final DeferredBlock<Block> BLUEBERRY_BUSH = block(
             "blueberry_bush",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> CORN_CROP = block(
             "corn_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> HOPS_CROP = block(
             "hops_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> HOT_PEPPERS_CROP = block(
             "hot_peppers_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> PINK_MELON_CROP = block(
             "pink_melon_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> RADISH_CROP = block(
             "radish_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> RED_CABBAGE_CROP = block(
             "red_cabbage_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> STARFRUIT_CROP = block(
             "starfruit_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> SUMMER_SPANGLE_CROP = block(
             "summer_spangle_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> SUMMER_SQUASH_CROP = block(
             "summer_squash_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
 
@@ -142,61 +237,111 @@ public class SDBlocks {
     public static final DeferredBlock<Block> AMARANTH_CROP = block(
             "amaranth_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> ARTICHOKE_CROP = block(
             "artichoke_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> BOK_CHOY_CROP = block(
             "bok_choy_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> BROCCOLI_CROP = block(
             "broccoli_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> CRANBERRY_BUSH = block(
             "cranberry_bush",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> EGGPLANT_CROP = block(
             "eggplant_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> FAIRY_ROSE_CROP = block(
             "fairy_rose_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> GRAPES_CROP = block(
             "grapes_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> SUGAR_PUMPKIN_CROP = block(
             "sugar_pumpkin_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> YAM_CROP = block(
             "yam_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
 
@@ -204,7 +349,12 @@ public class SDBlocks {
     public static final DeferredBlock<Block> POWDERMELON_CROP = block(
             "powdermelon_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
 
@@ -212,31 +362,56 @@ public class SDBlocks {
     public static final DeferredBlock<Block> ANCIENT_FRUIT_CROP = block(
             "ancient_fruit_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> QI_FRUIT_CROP = block(
             "qi_fruit_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> SWEET_GEM_BERRY_CROP = block(
             "sweet_gem_berry_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> PINEAPPLE_CROP = block(
             "pineapple_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
     public static final DeferredBlock<Block> TARO_ROOT_CROP = block(
             "taro_root_crop",
             Block::new,
-            p -> p
+            p -> p.mapColor(MapColor.PLANT)
+                    .noCollission()
+                    //.randomTicks()
+                    .instabreak()
+                    .sound(SoundType.CROP)
+                    .pushReaction(PushReaction.DESTROY)
     );
 
 
