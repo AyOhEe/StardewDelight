8a9eb5eb9a9439ed89cf8f607a0b481e03a35c83
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-08-21 01:33:05 +0100

    Some work on item tags

--- src/main/java/io/github/ayohee/stardewdelight/SDTags.java
+++ src/main/java/io/github/ayohee/stardewdelight/SDTags.java
@@ -100,6 +100,67 @@ public class SDTags {
         public static final TagKey<Item> APPLE_LOGS = create("apple_logs");
         public static final TagKey<Item> POMEGRANATE_LOGS = create("pomegrate_logs");
 
+
+        public static final TagKey<Item> FRUIT_TREE_LOGS = create("fruit_tree_logs");
+        public static final TagKey<Item> FRUIT_TREE_WOOD = create("fruit_tree_wood");
+        public static final TagKey<Item> FRUIT_TREE_STRIPPED_LOGS = create("fruit_tree_stripped_logs");
+        public static final TagKey<Item> FRUIT_TREE_STRIPPED_WOOD = create("fruit_tree_stripped_wood");
+        public static final TagKey<Item> FRUIT_TREE_PLANKS = create("fruit_tree_planks");
+        public static final TagKey<Item> FRUIT_TREE_STAIRS = create("fruit_tree_stairs");
+        public static final TagKey<Item> FRUIT_TREE_SLABS = create("fruit_tree_slabs");
+        public static final TagKey<Item> FRUIT_TREE_FENCES = create("fruit_tree_fences");
+        public static final TagKey<Item> FRUIT_TREE_FENCE_GATES = create("fruit_tree_fence_gates");
+        public static final TagKey<Item> FRUIT_TREE_DOORS = create("fruit_tree_doors");
+        public static final TagKey<Item> FRUIT_TREE_TRAPDOORS = create("fruit_tree_trapdoors");
+        public static final TagKey<Item> FRUIT_TREE_PRESSURE_PLATES = create("fruit_tree_pressure_plates");
+        public static final TagKey<Item> FRUIT_TREE_BUTTONS = create("fruit_tree_buttons");
+        public static final TagKey<Item> FRUIT_TREE_LEAVES = create("fruit_tree_leaves");
+
+        public static final TagKey<Item> FRUIT_TREE_SAPLINGS = create("fruit_tree_saplings");
+
+
+        public static final TagKey<Item> SMALL_FLOWERS = create("small_flowers");
+        public static final TagKey<Item> TALL_FLOWERS = create("tall_flowers");
+
+
+        public static final TagKey<Item> SD_STORAGE_BLOCKS = create("storage_blocks");
+
+        public static final TagKey<Item> STORAGE_BLOCKS_BLUE_JAZZ = common("storage_blocks/blue_jazz");
+        public static final TagKey<Item> STORAGE_BLOCKS_CAULIFLOWER = common("storage_blocks/cauliflower");
+        public static final TagKey<Item> STORAGE_BLOCKS_COFFEE = common("storage_blocks/coffee");
+        public static final TagKey<Item> STORAGE_BLOCKS_GARLIC = common("storage_blocks/garlic");
+        public static final TagKey<Item> STORAGE_BLOCKS_GREEN_BEAN = common("storage_blocks/green_bean");
+        public static final TagKey<Item> STORAGE_BLOCKS_KALE = common("storage_blocks/kale");
+        public static final TagKey<Item> STORAGE_BLOCKS_PARSNIP = common("storage_blocks/parsnip");
+        public static final TagKey<Item> STORAGE_BLOCKS_RHUBARB = common("storage_blocks/rhubarb");
+        public static final TagKey<Item> STORAGE_BLOCKS_STRAWBERRY_PUNNET = common("storage_blocks/strawberry_punnet");
+        public static final TagKey<Item> STORAGE_BLOCKS_BLUEBERRY_PUNNET = common("storage_blocks/blueberry_punnet");
+        public static final TagKey<Item> STORAGE_BLOCKS_CORN = common("storage_blocks/corn");
+        public static final TagKey<Item> STORAGE_BLOCKS_HOPS = common("storage_blocks/hops");
+        public static final TagKey<Item> STORAGE_BLOCKS_HOT_PEPPER = common("storage_blocks/hot_pepper");
+        public static final TagKey<Item> STORAGE_BLOCKS_PINK_MELON = common("storage_blocks/pink_melon");
+        public static final TagKey<Item> STORAGE_BLOCKS_RADISH = common("storage_blocks/radish");
+        public static final TagKey<Item> STORAGE_BLOCKS_RED_CABBAGE = common("storage_blocks/red_cabbage");
+        public static final TagKey<Item> STORAGE_BLOCKS_STARFRUIT = common("storage_blocks/starfruit");
+        public static final TagKey<Item> STORAGE_BLOCKS_SUMMER_SPANGLE = common("storage_blocks/summer_spangle");
+        public static final TagKey<Item> STORAGE_BLOCKS_AMARANTH = common("storage_blocks/amaranth");
+        public static final TagKey<Item> STORAGE_BLOCKS_ARTICHOKE = common("storage_blocks/artichoke");
+        public static final TagKey<Item> STORAGE_BLOCKS_BOK_CHOY = common("storage_blocks/bok_choy");
+        public static final TagKey<Item> STORAGE_BLOCKS_BROCCOLI = common("storage_blocks/broccoli");
+        public static final TagKey<Item> STORAGE_BLOCKS_CRANBERRY_PUNNET = common("storage_blocks/cranberry_punnet");
+        public static final TagKey<Item> STORAGE_BLOCKS_EGGPLANT = common("storage_blocks/eggplant");
+        public static final TagKey<Item> STORAGE_BLOCKS_FAIRY_ROSE = common("storage_blocks/fairy_rose");
+        public static final TagKey<Item> STORAGE_BLOCKS_GRAPE = common("storage_blocks/grape");
+        public static final TagKey<Item> STORAGE_BLOCKS_SUGAR_PUMPKIN = common("storage_blocks/sugar_pumpkin");
+        public static final TagKey<Item> STORAGE_BLOCKS_YAM = common("storage_blocks/yam");
+        public static final TagKey<Item> STORAGE_BLOCKS_POWDERMELON = common("storage_blocks/powdermelon");
+        public static final TagKey<Item> STORAGE_BLOCKS_ANCIENT_FRUIT = common("storage_blocks/ancient_fruit");
+        public static final TagKey<Item> STORAGE_BLOCKS_QI_FRUIT = common("storage_blocks/qi_fruit");
+        public static final TagKey<Item> STORAGE_BLOCKS_SWEET_GEM_BERRY = common("storage_blocks/sweet_gem_berry");
+        public static final TagKey<Item> STORAGE_BLOCKS_PINEAPPLE = common("storage_blocks/pineapple");
+        public static final TagKey<Item> STORAGE_BLOCKS_TARO_ROOT = common("storage_blocks/taro_root");
+
+
         public static final Map<WoodType, TagKey<Item>> LOG_TAGS = Map.of(
                 SDBlocks.SDWoodTypes.APRICOT, APRICOT_LOGS,
                 SDBlocks.SDWoodTypes.BANANA, BANANA_LOGS,
@@ -112,6 +173,10 @@ public class SDTags {
 
         private static TagKey<Item> create(String name) {
             return TagKey.create(Registries.ITEM, StardewDelight.modLoc(name));
+        }
+
+        private static TagKey<Item> common(String name) {
+            return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", name));
         }
     }
 
--- src/main/java/io/github/ayohee/stardewdelight/datagen/SDItemTagsProvider.java
+++ src/main/java/io/github/ayohee/stardewdelight/datagen/SDItemTagsProvider.java
@@ -2,11 +2,14 @@ package io.github.ayohee.stardewdelight.datagen;
 
 import io.github.ayohee.stardewdelight.SDTags;
 import io.github.ayohee.stardewdelight.StardewDelight;
+import io.github.ayohee.stardewdelight.register.SDBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nullable;
@@ -17,8 +20,11 @@ public class SDItemTagsProvider extends ItemTagsProvider {
         super(output, provider, blockTagProvider, StardewDelight.MODID, existingFileHelper);
     }
 
+
+    // TODO FINISH
     @Override
     protected void addTags(HolderLookup.Provider provider) {
+        /*----- FRUIT TREE TAGS -----*/
         this.copy(SDTags.BlockTags.APRICOT_LOGS, SDTags.ItemTags.APRICOT_LOGS);
         this.copy(SDTags.BlockTags.BANANA_LOGS, SDTags.ItemTags.BANANA_LOGS);
         this.copy(SDTags.BlockTags.MANGO_LOGS, SDTags.ItemTags.MANGO_LOGS);
@@ -26,5 +32,96 @@ public class SDItemTagsProvider extends ItemTagsProvider {
         this.copy(SDTags.BlockTags.PEACH_LOGS, SDTags.ItemTags.PEACH_LOGS);
         this.copy(SDTags.BlockTags.APPLE_LOGS, SDTags.ItemTags.APPLE_LOGS);
         this.copy(SDTags.BlockTags.POMEGRANATE_LOGS, SDTags.ItemTags.POMEGRANATE_LOGS);
+
+
+        /*----- COMMON LOG TAGS -----*/
+
+
+        /*----- LOG BLOCK TAGS -----*/
+        /*----- WOOD BLOCK TAGS -----*/
+        /*----- STRIPPED LOG TAGS -----*/
+        /*----- STRIPPED WOOD TAGS -----*/
+        /*----- PLANK TAGS -----*/
+        /*----- STAIR TAGS -----*/
+        /*----- SLAB TAGS -----*/
+        /*----- FENCE TAGS -----*/
+        /*----- FENCE GATE TAGS -----*/
+        /*----- DOOR TAGS -----*/
+        /*----- TRAPDOOR TAGS -----*/
+        /*----- PRESSURE PLATE TAGS -----*/
+        /*----- BUTTON TAGS -----*/
+        /*----- LEAVES TAGS -----*/
+
+
+
+
+        /*----- SAPLING TAGS -----*/
+        copy(SDTags.BlockTags.FRUIT_TREE_SAPLINGS, SDTags.ItemTags.FRUIT_TREE_SAPLINGS);
+
+        tag(ItemTags.SAPLINGS).addTag(SDTags.ItemTags.FRUIT_TREE_SAPLINGS);
+
+
+        /*----- FLOWER ITEM TAGS -----*/
+        copy(SDTags.BlockTags.SMALL_FLOWERS, SDTags.ItemTags.SMALL_FLOWERS);
+        copy(SDTags.BlockTags.TALL_FLOWERS, SDTags.ItemTags.TALL_FLOWERS);
+
+        tag(ItemTags.SMALL_FLOWERS).addTag(SDTags.ItemTags.SMALL_FLOWERS);
+        tag(ItemTags.FLOWERS).addTag(SDTags.ItemTags.SMALL_FLOWERS);
+        tag(ItemTags.BEE_FOOD).addTag(SDTags.ItemTags.SMALL_FLOWERS);
+        tag(Tags.Items.ANIMAL_FOODS).addTag(SDTags.ItemTags.SMALL_FLOWERS);
+
+        tag(ItemTags.TALL_FLOWERS).addTag(SDTags.ItemTags.TALL_FLOWERS);
+        tag(ItemTags.FLOWERS).addTag(SDTags.ItemTags.TALL_FLOWERS);
+        tag(ItemTags.BEE_FOOD).addTag(SDTags.ItemTags.TALL_FLOWERS);
+        tag(Tags.Items.ANIMAL_FOODS).addTag(SDTags.ItemTags.TALL_FLOWERS);
+
+
+        /*----- CRATE TAGS -----*/
+        copy(SDTags.BlockTags.SD_STORAGE_BLOCKS, SDTags.ItemTags.SD_STORAGE_BLOCKS);
+
+        tag(Tags.Items.STORAGE_BLOCKS).addTag(SDTags.ItemTags.SD_STORAGE_BLOCKS);
+
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_BLUE_JAZZ).add(SDBlocks.BLUE_JAZZ_BOX.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_CAULIFLOWER).add(SDBlocks.CAULIFLOWER_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_COFFEE).add(SDBlocks.COFFEE_BAG.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_GARLIC).add(SDBlocks.GARLIC_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_GREEN_BEAN).add(SDBlocks.GREEN_BEAN_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_KALE).add(SDBlocks.KALE_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_PARSNIP).add(SDBlocks.PARSNIP_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_RHUBARB).add(SDBlocks.RHUBARB_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_STRAWBERRY_PUNNET).add(SDBlocks.STRAWBERRY_PUNNET_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_BLUEBERRY_PUNNET).add(SDBlocks.BLUEBERRY_PUNNET_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_CORN).add(SDBlocks.CORN_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_HOPS).add(SDBlocks.HOPS_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_HOT_PEPPER).add(SDBlocks.HOT_PEPPER_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_PINK_MELON).add(SDBlocks.PINK_MELON_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_RADISH).add(SDBlocks.RADISH_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_RED_CABBAGE).add(SDBlocks.RED_CABBAGE_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_STARFRUIT).add(SDBlocks.STARFRUIT_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_SUMMER_SPANGLE).add(SDBlocks.SUMMER_SPANGLE_BOX.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_AMARANTH).add(SDBlocks.AMARANTH_BALE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_ARTICHOKE).add(SDBlocks.ARTICHOKE_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_BOK_CHOY).add(SDBlocks.BOK_CHOY_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_BROCCOLI).add(SDBlocks.BROCCOLI_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_CRANBERRY_PUNNET).add(SDBlocks.CRANBERRY_PUNNET_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_EGGPLANT).add(SDBlocks.EGGPLANT_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_FAIRY_ROSE).add(SDBlocks.FAIRY_ROSE_BOX.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_GRAPE).add(SDBlocks.GRAPE_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_SUGAR_PUMPKIN).add(SDBlocks.SUGAR_PUMPKIN_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_YAM).add(SDBlocks.YAM_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_POWDERMELON).add(SDBlocks.POWDERMELON_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_ANCIENT_FRUIT).add(SDBlocks.ANCIENT_FRUIT_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_QI_FRUIT).add(SDBlocks.QI_FRUIT_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_SWEET_GEM_BERRY).add(SDBlocks.SWEET_GEM_BERRY_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_PINEAPPLE).add(SDBlocks.PINEAPPLE_CRATE.getItem().get());
+        tag(SDTags.ItemTags.STORAGE_BLOCKS_TARO_ROOT).add(SDBlocks.TARO_ROOT_CRATE.getItem().get());
+
+
+        /*----- SEED TAGS -----*/
+
+
+        /*----- CROP ITEM TAGS -----*/
+
+
     }
 }
