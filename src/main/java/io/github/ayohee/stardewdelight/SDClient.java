57f7e310531d0d9d27300d13efc94f85ac1ab4a2
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-08-24 00:51:06 +0100

    Seed tooltips

--- src/main/java/io/github/ayohee/stardewdelight/SDClient.java
+++ src/main/java/io/github/ayohee/stardewdelight/SDClient.java
@@ -1,16 +1,22 @@
 package io.github.ayohee.stardewdelight;
 
 import io.github.ayohee.stardewdelight.register.SDBlockEntityTypes;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.blockentity.HangingSignRenderer;
 import net.minecraft.client.renderer.blockentity.SignRenderer;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 import static io.github.ayohee.stardewdelight.StardewDelight.MODID;
+import static net.minecraft.ChatFormatting.GRAY;
 
 @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class SDClient {
@@ -18,9 +24,24 @@ public class SDClient {
     public static void onClientSetup(FMLClientSetupEvent event) {
         // Yet empty.
     }
+
     @SubscribeEvent
     public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerBlockEntityRenderer(SDBlockEntityTypes.SIGN.get(), SignRenderer::new);
         event.registerBlockEntityRenderer(SDBlockEntityTypes.HANGING_SIGN.get(), HangingSignRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void addToItemTooltip(ItemTooltipEvent event) {
+        if (event.getEntity() == null)
+            return;
+
+        Item item = event.getItemStack().getItem();
+        ResourceLocation location = BuiltInRegistries.ITEM.getKey(item);
+
+        String componentName = "item." + location.getNamespace() + "." + location.getPath() + ".tooltip";
+        if (I18n.exists(componentName)) {
+            event.getToolTip().add(1, Component.translatable(componentName).withStyle(GRAY));
+        }
     }
 }
