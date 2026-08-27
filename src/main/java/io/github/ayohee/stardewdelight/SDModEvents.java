4a32eb6bf3efdce239baddf4382f8feefcba9de6
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2026-08-28 00:42:15 +0100

    Farmer trade for seed packets
    Also fixes rare seed/qi bean limits

--- src/main/java/io/github/ayohee/stardewdelight/SDModEvents.java
+++ src/main/java/io/github/ayohee/stardewdelight/SDModEvents.java
@@ -1,10 +1,12 @@
 package io.github.ayohee.stardewdelight;
 
 import io.github.ayohee.stardewdelight.register.SDItems;
+import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.event.village.VillagerTradesEvent;
 import net.neoforged.neoforge.event.village.WandererTradesEvent;
 
 @EventBusSubscriber(modid = StardewDelight.MODID)
@@ -16,7 +18,14 @@ public class SDModEvents {
 
     @SubscribeEvent
     private static void onWandererTrades(WandererTradesEvent event) {
-        event.getRareTrades().add(new VillagerTrades.ItemsForEmeralds(SDItems.RARE_SEED.asItem(), 5, 1, 1));
-        event.getRareTrades().add(new VillagerTrades.ItemsForEmeralds(SDItems.QI_BEAN.asItem(), 5, 1, 1));
+        event.getRareTrades().add(new VillagerTrades.ItemsForEmeralds(SDItems.RARE_SEED.asItem(), 5, 1, 1, 1));
+        event.getRareTrades().add(new VillagerTrades.ItemsForEmeralds(SDItems.QI_BEAN.asItem(), 5, 1, 1, 1));
+    }
+
+    @SubscribeEvent
+    private static void onVillagerTrades(VillagerTradesEvent event) {
+        if (event.getType() == VillagerProfession.FARMER) {
+            event.getTrades().get(1).add(new VillagerTrades.ItemsForEmeralds(SDItems.SEED_PACKET.asItem(), 2, 1, 3, 1));
+        }
     }
 }
